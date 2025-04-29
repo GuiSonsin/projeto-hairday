4b4fb8f21bd1f61bab5327c52059b318cdbbdf23
GuiSonsin <sonsinguilherme@gmail.com>
2025-04-29 15:23:55 -0300

    Selecionando um horário

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs"
+
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
@@ -36,6 +38,8 @@ export function hoursLoad( { date } ){
 
         hours.append(li)
     })
+
+    hoursClick()
 }
 
 
